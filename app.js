1422d05349f1bfb8de443b4ca68a3afb85acf28f
Mujtaba1i <unmujt@gmail.com>
2025-11-11 11:38:19 +0300

    EXE3

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
 /*
+Exercise 3: isCharAVowel()
+
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true.
+
+Complete the exercise in the space below:
+*/
+
+const isCharAVowel = charecter => {
+    if (charecter === 'a' || charecter === 'e' || charecter === 'i' || charecter === 'o' || charecter === 'u' )
+    {
+        return true
+    }
+    else
+    {
+        return false
+    }
+    }
+
+console.log('Exercise 3 Result:', isCharAVowel("a"));
+
+
+
+/*
 Exercise 4: generateEmail()
 
 Create a function named generateEmail. It should take two strings: 
